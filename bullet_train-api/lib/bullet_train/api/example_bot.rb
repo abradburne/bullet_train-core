c9c0c4dddeb92ed13cdbd57864c7ea676de071ea
Alan Bradburne <abradburne@gmail.com>
2023-08-30 13:40:04 +0900

    patches to get doc gen working with ruby 2.7

--- bullet_train-api/lib/bullet_train/api/example_bot.rb
+++ bullet_train-api/lib/bullet_train/api/example_bot.rb
@@ -39,7 +39,9 @@ module FactoryBot
       objects
     end
 
-    %i[get_examples get_example post_examples post_parameters put_example put_parameters patch_example patch_parameters].each do |method|
+    REST_METHODS = %i[get_examples get_example post_example post_parameters put_example put_parameters patch_example patch_parameters]
+
+    REST_METHODS.each do |method|
       define_method(method) do |model, **options|
         _path_examples(method.to_s, model, **options)
       end
@@ -53,6 +55,7 @@ module FactoryBot
     end
 
     def reset_tables!
+      return unless ActiveRecord::Base.connection.respond_to?(:reset_pk_sequence!)
       @tables_to_reset.each do |name|
         ActiveRecord::Base.connection.reset_pk_sequence!(name) if ActiveRecord::Base.connection.table_exists?(name)
       end
@@ -67,8 +70,12 @@ module FactoryBot
           clone.send("#{name}=", associations)
           @tables_to_reset << name
         elsif %i[belongs_to has_one].include?(reflection.macro)
-          clone.send("#{name}=", instance.send(name).clone)
-          @tables_to_reset << name.pluralize
+          begin
+            clone.send("#{name}=", instance.send(name).clone)
+            @tables_to_reset << name.pluralize
+          rescue ActiveRecord::HasOneThroughNestedAssociationsAreReadonly
+            puts "skipping #{name} as readonly"
+          end
         end
       end
 
